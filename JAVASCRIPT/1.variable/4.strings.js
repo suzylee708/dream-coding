9b95db63175a37504a055568e3d6afd4ffa9225a
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-07 23:03:20 -0400

    Create 4.strings.js

---
+++ JAVASCRIPT/1.variable/4.strings.js
@@ -0,0 +1,19 @@
+//string
+
+let string = "something";
+
+string = `hello`;
+console.log(string);
+
+string = 'hello!\n This is Suji \t\t Lee'
+console.log(string)
+
+// \t = tab
+// \n = Enter
+
+// Template Literal ` `
+
+let id = 'Suji';
+let greetings = `'hello, ${id}
+have a great day!'`
+console.log(greetings)
