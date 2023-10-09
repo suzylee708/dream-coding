c05b747fe045cd6146b0a2f429cff43bbdcd3d12
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-09 16:32:32 -0400

    Create 10.typeof.js

---
+++ JAVASCRIPT/1.variable/10.typeof.js
@@ -0,0 +1,13 @@
+console.clear();
+
+let variable;
+console.log(typeof variable);
+
+variable = '' //string
+console.log(typeof variable);
+
+variable = 123; //number
+console.log(typeof variable);
+
+variable = {}; // object
+console.log(typeof variable);
