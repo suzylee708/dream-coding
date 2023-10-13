8c05a15809e06164b3971f3afe3a4f4a180427d0
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-13 11:46:32 -0400

    Create 1.basic.js

---
+++ JAVASCRIPT/1.basic.js
@@ -0,0 +1,15 @@
+function add (a, b) {
+    return a + b;
+}
+const result = add(1, 2);
+console.log(result); // 3
+
+
+function fullName(firstName, lastName){
+    return `${firstName} ${lastName}👍`;
+}
+// let lastName = 'Lee';
+// let firstName = 'Suji';
+console.log(fullName('Suji', 'Park'));
+console.log(fullName('Suji', 'Lee'));
+
