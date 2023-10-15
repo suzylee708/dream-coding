645bcaf1ac71dd0c32d8e186b5a4bd8fdbbaafcb
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-14 23:51:18 -0400

    Create 3.unary.js

---
+++ JAVASCRIPT/2.operator/3.unary.js
@@ -0,0 +1,25 @@
+// Unary Operators 단항연산자
+
+// + (양) operator
+// - (음) operator
+// ! (부정) operator
+
+let a = 5;
+a = -a; // -1 *5
+console.log(a)//-5
+console.log(-a)//5
+
+a = +a;
+console.log(a) //-5
+a = -a;
+a = +a;
+console.log(a)
+
+let boolean = true;
+console.log(boolean); //true
+console.log(!boolean); //false
+console.log(!!boolean); //true
+
+// + 
+
+console.clear();
