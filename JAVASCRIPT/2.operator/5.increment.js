ab317c76f67ab90f951c11d9e94125e5d1988ca0
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-17 00:01:39 -0400

    Create 5.increment.js

---
+++ JAVASCRIPT/2.operator/5.increment.js
@@ -0,0 +1,14 @@
+//증가 와 감소 연산자 
+//Increment & Decrement Operators
+
+let a =0;
+console.log(a)
+a++; //a = a +1
+console.log(a)
+
+// 주의
+a = 0;
+// console.log(++a);
+let b = ++a;
+console.log(b);
+console.log(a);
