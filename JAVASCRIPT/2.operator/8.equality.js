cefe623da5475fcc519ad90c6df8887518822880
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-17 22:29:18 -0400

    Create 8.equality.js

---
+++ JAVASCRIPT/2.operator/8.equality.js
@@ -0,0 +1,10 @@
+//동등 비교 관계 연산자 (Equality operators)
+
+console.log(2 == 2); //True
+console.log(2 != 2); // False
+console.log(2 != 3); // True
+console.log(2 == 3); // False
+console.log(2 == '2'); // True
+console.log(2 === '2'); // False
+
+console.clear();
