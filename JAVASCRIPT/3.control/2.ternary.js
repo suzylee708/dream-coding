057d12471487acdf048efee5132a262a9c4a82e5
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-23 10:56:00 -0400

    Create 2.ternary.js

---
+++ JAVASCRIPT/3.control/2.ternary.js
@@ -0,0 +1,13 @@
+// Ternary 삼항 조건 연산자
+// 조건식 ? 참인경우 : 거짓인 경우
+
+let fruit = 'apple';
+if(fruit === 'apple') {
+    console.log('🍎');
+} else{
+    console.log('😊');
+}
+
+fruit === 'apple' ? console.log('🍎') : console.log('😊');
+let emoji = fruit === 'apple' ? '🍎' : '😊'
+
