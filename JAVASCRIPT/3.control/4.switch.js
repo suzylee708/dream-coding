840b8367a1b826906c5a1049685e81b07af81e4f
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-24 23:54:17 -0400

    Create 4.switch.js

---
+++ JAVASCRIPT/3.control/4.switch.js
@@ -0,0 +1,29 @@
+// Switch 도 조건문에 해당한다. Conditional Statement
+
+// 정해진 범위안의 값에 대해 특정한 일을 해야하는 경우
+let day = 6;
+let dayName;
+
+switch(day) {
+    case 0:
+        dayName = 'Monday'
+        break;
+    case 1:
+        dayName = 'Tuesday'
+        break;
+    case 2:
+        dayName = 'Wednesday'
+        break;
+    case 3:
+        dayName = 'Thursday'
+        break;
+    case 4:
+        dayName = 'Friday'
+        break;
+    case 5:
+        dayName = 'Saturday'
+        break;
+    case 6:
+        dayName = 'Sunday'
+        break;
+}
