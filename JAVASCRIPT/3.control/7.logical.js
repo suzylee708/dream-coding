d86fb0473bbb45d0051f63a4aa9724b53357dd37
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-10-30 23:56:33 -0400

    Create 7.logical.js

---
+++ JAVASCRIPT/3.control/7.logical.js
@@ -0,0 +1,20 @@
+// 논리 연산자 Logical operator
+
+// && and
+// || or
+// ! 부정 연산자
+// !! 특정한 값을 boolean 값으로 변환
+
+
+let num = 8;
+if(num <9 || num < 5){
+    console.log('👍');
+}
+if(num != 7){
+    console.log('😍');
+}
+
+
+console.log('text'); // text
+console.log(!'text');// false
+console.log(!!'text');//true
