871684d66232622abb2b138f5e20c03e68eca5fe
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-11-03 21:16:49 -0400

    Create 6.callback.js

---
+++ JAVASCRIPT/4.function/6.callback.js
@@ -0,0 +1,14 @@
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+
+function calculator(a, b, action) {
+  if (a < 0 || b < 0) {
+    return;
+  }
+  let result = action(a, b);
+  console.log(result);
+  return result;
+}
+
+calculator(1, 3, add);
+calculator(1, -3, multiply);
