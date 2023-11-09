579a417f7883d4812f6989013ec3538b27ce9243
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-11-09 09:43:26 -0500

    Create 1.wrapper.js

---
+++ JAVASCRIPT/1.wrapper.js
@@ -0,0 +1,11 @@
+// Wrapper Object 래퍼 객체
+// 원시값을 필요에 따라서 관련된 빌트인 객체로 변환한다.
+const number = 123; //number 원시타입
+// number 원시타입을 감싸고 있는 Number 객체로 감싸짐
+console.log(number.toString());
+console.log(number); //number 원시타입
+
+const text = 'text';
+console.log(text);
+text.length;
+text.trim();
