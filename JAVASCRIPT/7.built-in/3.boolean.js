e767a6dfe360d53b19a5d74e77e60ae033649279
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-11-22 00:58:22 -0500

    Create 3.boolean.js

---
+++ JAVASCRIPT/7.built-in/3.boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+const isTrue = true;
+console.log(isTrue.valueOf());
+
+/**
+ *Falshy
+ 0
+ -0
+ ''
+ NaN
+ null
+ undefined
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
