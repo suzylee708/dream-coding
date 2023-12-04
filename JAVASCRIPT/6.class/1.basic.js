e4737dc2ea94fca0913ca574d460a35ef0e851ff
Suji Lee <137566589+suzylee708@users.noreply.github.com>
2023-12-03 22:12:54 -0500

    Create 1.basic.js

---
+++ JAVASCRIPT/6.class/1.basic.js
@@ -0,0 +1,13 @@
+//class
+
+class Fruit {
+  constructor(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+  }
+  display() {
+    console.log(`${this.name}: ${this.emoji}`);
+  }
+}
+
+const apple = new Fruit('apple', '🍎');
